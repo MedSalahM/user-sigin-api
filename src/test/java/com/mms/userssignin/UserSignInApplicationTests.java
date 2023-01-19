30351ebc2277b5278ca4048a8b5e7b4d66ce8917
MedSalahM <59416657+MedSalahM@users.noreply.github.com>
2023-01-19 14:21:12 +0100

    change java version

--- src/test/java/com/mms/userssignin/UserSignInApplicationTests.java
+++ src/test/java/com/mms/userssignin/UserSignInApplicationTests.java
@@ -2,8 +2,12 @@ package com.mms.userssignin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.mms.users.UserSignInApplication;
 
 @SpringBootTest
+@ContextConfiguration(classes = UserSignInApplication.class)
 class UserSignInApplicationTests {
 
 	@Test
